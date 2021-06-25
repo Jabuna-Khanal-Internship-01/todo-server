e1cfce1d8d6955eac745cce20387ceb60da0c691
jabuna07 <jabuna007@gmail.com>
2021-06-25 12:53:45 +0545

    added dotenv file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 import toDoRoutes from './routes/todo.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
@@ -11,9 +13,9 @@ app.use(cors());
 app.use('/todo', toDoRoutes);
 
 const PORT = process.env.PORT || 4000 ;
-const CONNECTION_URL ="mongodb+srv://jabuna:56charpala@cluster0.rkjtd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=> app.listen(PORT, ()=> console.log(`Server running on PORT :${PORT}`)))
 .catch((error) => console.log(error.message));
 
